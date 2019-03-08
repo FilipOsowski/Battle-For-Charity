9522289da7d100917e6123add3787ee76e784f5f
Eddie Federmeyer <eddiefedermeyer@yahoo.com>
2019-03-08 14:36:25 -0600

    react works now

--- frontpage/src/index.js
+++ frontpage/src/index.js
@@ -1,33 +1,39 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-
-// class Bracket extends React.Component {
-//     render() {
-//         return(
-//
-//         )
-//     }
-// }
-
 class Page extends React.Component {
     render() {
         return(
-            <div className="leaderboard list">
-                <h1>Leaderboard</h1>
-                <ul>
-                    <li>First</li>
-                    <li>Second</li>
-                    <li>Third</li>
-                    <li>Fourth</li>
-                    <li>Fifth</li>
-                </ul>
+            <div className = "mainpage">
+                <div className = "tournamentBracket">
+
+                </div>
+                <div className="leaderboardList">
+                    <h1>Leaderboard</h1>
+                    <ul>
+                        <li>First</li>
+                        <li>Second</li>
+                        <li>Third</li>
+                        <li>Fourth</li>
+                        <li>Fifth</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+//These are the buttons people will click to set the winner of the fight
+class RadioButton extends React.Component {
+    setWinner(event) {
+        console.log(event.target.value);
+    }
+
+    render() {
+        return(
+            <div onChange={this.setWinner.bind(this)}>
+                Person1 <input type="radio" value="Person1" name="winner"/>
             </div>
         )
     }
 }
 
 ReactDOM.render(<Page />, document.getElementById("root"));
-serviceWorker.unregister();
+// serviceWorker.unregister();
