ca6b8d6bb4e7e1f7625e4292d05dae15fb8a9169
Filip Osowski <filiposowski5@gmail.com>
2019-05-02 03:11:02 +0000

    Add login & main builds

---
+++ login_build/precache-manifest.f16c29b73856edeca7d357310ea48365.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a2e07610942a67fa606",
+    "url": "/static/css/main.2d0c4637.chunk.css"
+  },
+  {
+    "revision": "5a2e07610942a67fa606",
+    "url": "/static/js/main.fd5a8174.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f177bf24fa7be945a5f9",
+    "url": "/static/js/2.f59f2440.chunk.js"
+  },
+  {
+    "revision": "5ae3751bb660975575828dce029b5ac3",
+    "url": "/index.html"
+  }
+];
---
+++ main_build/precache-manifest.c7be54f6474a1a64141681c12c7b736d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ec5ee2500458655c00",
+    "url": "/static/css/main.fbec1511.chunk.css"
+  },
+  {
+    "revision": "a6ec5ee2500458655c00",
+    "url": "/static/js/main.d0b2135a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e8806030e7b6a5a1678",
+    "url": "/static/js/2.3faadd0f.chunk.js"
+  },
+  {
+    "revision": "daa27111055e81cb51c711606a48c47a",
+    "url": "/index.html"
+  }
+];
