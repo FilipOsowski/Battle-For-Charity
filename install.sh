abb0991ffd758452c247a5dc8fe5d8ca38acd7f5
root <root@ip-172-31-38-62.us-east-2.compute.internal>
2019-05-02 13:48:16 +0000

    Add unfinished install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+if [[ $UID != 0 ]]; then
+	echo "Please run this script with sudo:"
+	echo "sudo $0 $*"
+	exit 1
+else:
+	sudo apt install nginx mysql-server
+	wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+	sudo bash miniconda.sh -b
+	sudo rm miniconda.sh
+	git clone https://github.com/FilipOsowski/Battle-For-Charity
+	conda env list
+fi
